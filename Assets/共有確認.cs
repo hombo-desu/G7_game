cea015a3d5fdeffabf201c9070ca01244a38ce9d
hombo-desu <tmih22336@stu.tamagawa.ac.jp>
2023-12-21 13:39:33 +0900

    test

---
+++ Assets/共有確認.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 共有確認 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*===========================-
+         * 
+         * 泰真参上!!!!!!!!
+         * お宝はいただいていくぜ、とっつぁん
+         * 
+         * =============================*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
